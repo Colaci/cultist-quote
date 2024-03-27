00a29b539ab7d5e55d1de3dbbf14cd9bfef4d9db
wy <1311936071@qq.com>
2024-03-27 16:28:24 +0800

    test: delete test

--- app/testPage/page.tsx
+++
@@ -1,9 +0,0 @@
-// 不加use client 这默认是一个server component
-
-export default async function(){
-  const data = await fetch('http://localhost:3000/api/testAPI',{cache:"no-cache"})
-  const time = (await data.json()).data
-  return (<main>
-    {time}
-  </main>)
-}
